a4077c71b1f6171fd88d740913092a2c55231538
Jaime Tovar <jmtovar@gmail.com>
2014-10-29 11:45:07 +0000

    With cache expiring.

--- bathsite/data_providers/cache.py
+++ bathsite/data_providers/cache.py
@@ -1,16 +1,18 @@
 __author__ = 'Jofiel'
 from treegenerator.models import CachedURL
+from django.utils import timezone
 
 class CacheController(object) :
 
-    def tryCache(self, speciesList, database):
+    def tryCache(self, speciesList, database, minutes = 1):
         found = dict()
         notFound = []
         for species in speciesList :
             print("Trying cache for {}".format(species))
-            cachedList = CachedURL.objects.filter(species=species, database=database)
+
+            cachedList = CachedURL.objects.filter(species=species, database=database).filter(date__range = [timezone.now()-timezone.timedelta(minutes = minutes),timezone.now()])
             if len(cachedList) > 0 :
-                print(cachedList)
+                print("Found cache for {}".format(species))
                 found[species] = cachedList[0].url
             else :
                 notFound.append(species)
