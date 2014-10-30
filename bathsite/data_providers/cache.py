4d6aa740bc2af39cc57a19a0e861d74258916fea
Jaime Tovar <jmtovar@gmail.com>
2014-10-30 01:32:02 +0000

    Some docstrings for main things.

--- bathsite/data_providers/cache.py
+++ bathsite/data_providers/cache.py
@@ -2,15 +2,26 @@ __author__ = 'Jofiel'
 from treegenerator.models import CachedURL
 from django.utils import timezone
 
-class CacheController(object) :
+class CacheController(object):
+    """ Class to control cache access.
+    """
 
-    def tryCache(self, speciesList, database, minutes = 1):
+    def tryCache(self, speciesList, database, minutes = 60):
+        """
+        Tries to find if there is a previous register for the species/database in the cache. All data is kept in the
+        database. Probably needs some cleanup
+        :param speciesList: List of species names as they are queried against the database (with spaces in names).
+        :param database: Name of the database. Just the name as a string.
+        :param minutes: Number of minutes for cache expiry. Default 1 hour.
+        :return: dictionary with those species found in the cache species=>url, list of species not found in the cache.
+        """
         found = dict()
         notFound = []
         for species in speciesList :
             print("Trying cache for {}".format(species))
 
-            cachedList = CachedURL.objects.filter(species=species, database=database).filter(date__range = [timezone.now()-timezone.timedelta(minutes = minutes),timezone.now()])
+            cachedList = CachedURL.objects.filter(species=species, database=database)\
+                .filter(date__range = [timezone.now()-timezone.timedelta(minutes = minutes),timezone.now()])
             if len(cachedList) > 0 :
                 print("Found cache for {}".format(species))
                 found[species] = cachedList[0].url
@@ -19,6 +30,10 @@ class CacheController(object) :
         return found, notFound
 
     def storeCache(self, speciesDict, database):
+        """ Store values got from database in cache.
+        :param speciesDict: Dictionary with the species->url to save in cache
+        :param database: Name of the database where this urls were found.
+        """
         for species in speciesDict.keys() :
             print("Storing {} {}".format(species, speciesDict[species]))
             url = speciesDict[species]
