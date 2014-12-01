dee0b2dfbb234c842c52fc91813e6d72b90a11a1
Jorge Jair Ramírez Estrada <jorgejairsmln@hotmail.com>
2014-12-01 17:42:09 -0600

    Ping tests added

--- bathsite/tests/tests.py
+++ bathsite/tests/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 from django.test import Client
 
-class DestructiveTests(TestCase):
-    def begin_tests(self):
+class Tests(TestCase):
+    def setUp(self):
         c = Client()
+
+    def ping_tests(self):
         response = c.get('/ping/')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'pong')
+        
+    def destructive_test(self):
+        
